44f45942db8fec9a144961551cff5f7ecdd7fe21
xuliang <xuliang_1827@163.com>
2018-11-23 17:45:39 +0800

    strpng boot test

--- src/test/java/com/demon/spring_boot/ApplicationTests.java
+++ src/test/java/com/demon/spring_boot/ApplicationTests.java
@@ -1,16 +1,30 @@
 package com.demon.spring_boot;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+    
 	@Test
-	public void contextLoads() {
+	public void getHello() throws Exception {
+	    mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+	        .andExpect(MockMvcResultMatchers.status().isOk())
+	        .andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("Hello World.")));
 	}
 
 }
